b4958581b018472ca7c369c14e4224f80e1a563e
imrane010 <144110083+imrane010@users.noreply.github.com>
2023-11-10 11:42:07 +0100

    Add files via upload

---
+++ opdracht6.js
@@ -0,0 +1,26 @@
+// Oefening 1
+function calculate(num1, num2) {
+    var sum = num1 + num2;
+    console.log('Antwoord:', sum);
+}
+
+// Voorbeeld gebruik
+calculate(1, 12);
+
+// Oefening 2
+function multiplyByFive(num) {
+    var result = num * 5;
+    console.log('Antwoord:', result);
+}
+
+// Voorbeeld gebruik
+multiplyByFive(7);
+
+// Oefening 3
+function calculateMinutes(seconds) {
+    var minutes = seconds / 60;
+    console.log('Antwoord:', minutes);
+}
+
+// Voorbeeld gebruik
+calculateMinutes(3600);
