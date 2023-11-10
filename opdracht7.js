7cb3e37692f5b04745076ccd7df1e8e9c9c3d744
imrane010 <144110083+imrane010@users.noreply.github.com>
2023-11-10 11:46:54 +0100

    Add files via upload

---
+++ opdracht7.js
@@ -0,0 +1,17 @@
+var leeftijd = 18;
+
+if (leeftijd >= 18) {
+    console.log('Je bent volwassen.');
+} else {
+    console.log('Je bent minderjarig.');
+}
+var naam = 'John';
+console.log('Hallo, ' + naam + '!');
+
+var aantalAppels = 5;
+
+if (aantalAppels > 0) {
+    console.log('Er zijn appels beschikbaar.');
+} else {
+    console.log('Er zijn geen appels beschikbaar.');
+}
